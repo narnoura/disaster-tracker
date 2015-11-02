8844149b76e955f0e6bb47a514b4bd7654734260
MichaelPinkham <mlp2185@columbia.edu>
2015-11-02 14:45:00 -0500

    Refactored for better structure of saving/parsing articles. Code is 2x slower, but data is cleaner

--- unigram_stats.py
+++ unigram_stats.py
@@ -1,66 +1,29 @@
 import os
 import re
-import sys
-from glob import glob
 from ngram import NGram
 
 class UnigramStats(object):
 
-    def __init__(self, argv):
+    def __init__(self, corpus_parser):
         '''
         Collect relevant stats about unigrams
 
         Args:
-            argv (list): A list of arguments, passed from word_stats
-                - argv[0]: 'word_stats.py', typically
-                - argv[1]: The name of a directory containing .json
-                           articles to be analyzed
-                - argv[2]: The name of a stop-word list text file
+            corpus_parser: contains a list of articles and a dictionary of words
         '''
-        self.corpus_dir = [] #List of string article titles
+        self.corpus_dir = corpus_parser.corpus_dir #List of string article titles
+        self.articles = corpus_parser.articles
         self.word_dict = {}
-        self.stop_words = []
-        self.argv = argv
-
-
-    def read_from_dir(self):
-        arg = str(self.argv[1])
-        dir_name = self.clean_arg(arg)
-        self.corpus_dir = glob(dir_name + "*")
-            
-        return self.corpus_dir
-
-
-    def clean_arg(self, arg):
-        end_slash = "/"
-        if arg[len(arg) - 1] is '/':
-            return arg
-        else:
-            return (arg + end_slash)
+        self.stop_words = corpus_parser.stop_words
 
     
-    def fill_stop_words(self):
-        with open(str(self.argv[2])) as f:
-            for line in f:
-                self.stop_words.append(line.rstrip("\n"))
-        
-        return self.stop_words
+    def get_stats(self):
+        for article in self.articles:
+            self.parse_string(article)
 
-    
-    def parse_articles(self):
-        for filename in self.corpus_dir:
-            with open(filename) as f:
-                for line in f:
-                    self.parse_line(line)
-
-
-    def parse_line(self, line):
-        word_list = re.split("\W+", line)
+    def parse_string(self, string):
+        word_list = re.split("\W+", string)
         for word in word_list:
-            #clean_words = word.split("\n\n")
-            #for unigram in clean_words:
-             #   clean_unigram = unigram.strip(".,{}():\-!?\n\"")
-             #   print clean_unigram
             self.update_dict(word)
 
 
@@ -75,12 +38,10 @@ class UnigramStats(object):
 
 
     def print_dict(self):
-        sorted_values = sorted(self.word_dict.values(), key=lambda ngram: ngram.frequency)
+        sorted_values = sorted(self.word_dict.values(), 
+                               key=lambda ngram: ngram.frequency)
         for value in sorted_values:
             NGram.print_attrib(value)
 
-    def setup_query(self):
-        self.read_from_dir()
-        self.fill_stop_words()
-        self.parse_articles()
+    def print_stats(self):
         self.print_dict()
--- word_stats.py
+++ word_stats.py
@@ -1,8 +1,28 @@
 import sys
+from corpus_parser import CorpusParser
 from unigram_stats import UnigramStats
+from time import clock
 
-# Save single-word stats
-unigram_stats = UnigramStats(sys.argv)
+start = clock()
+
+# Pick the corpus to parse
+corpus_parser = CorpusParser(sys.argv)
+
+# Parse it 
+corpus_parser.parse()
+
+part1 = clock()
+
+
+# Get stats for the particular corpus
+unigram_stats = UnigramStats(corpus_parser)
 
 # Parse articles for content words and print their frequencies
-unigram_stats.setup_query()
+unigram_stats.get_stats()
+unigram_stats.print_stats()
+
+part2 = clock()
+
+print "\n"
+print (part1 - start)
+print (part2 - part1)
